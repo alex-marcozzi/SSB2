51edad6e90cdace01f86623cf3fe22ae11f69536
alex-marcozzi <alex.marcozzi1@gmail.com>
2021-05-23 13:49:09 -0500

    Implement basic pygame functionality

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-x = 10
+import sys, pygame
+pygame.init()
 
-for i in reversed(range(x)):
-    print(i+1)
+size = width, height = 800, 600
+speed = [1, 1]
+black = 0, 0, 0
+RED = (255, 0, 0)
 
-print("Welcome to Super Square Boy 2")
+screen = pygame.display.set_mode(size)
+
+# ball = pygame.image.load("intro_ball.gif")
+ballrect = pygame.Rect(50, 50, 80, 80) #ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    #screen.blit(ball, ballrect)
+    pygame.draw.rect(screen, RED, ballrect)
+    pygame.display.flip()
